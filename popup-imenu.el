378dfe02911315b81a3c562f6b929ca6f55e7fac
Igor Shymko <Igor.Shymko@playtech.com>
2015-12-29 18:48:56 +0200

    fix adding whitespaces issue

--- popup-imenu.el
+++ popup-imenu.el
@@ -36,7 +36,6 @@
 (require 'dash)
 (require 'popup)
 (require 'imenu)
-(require 'artist)
 (require 'flx-ido)
 
 (defvar popup-imenu-fuzzy-match t
@@ -45,11 +44,17 @@
 (defvar popup-imenu-hide-rescan t
   "Hide *Rescan* menu item.")
 
-(defvar popup-imenu-position 'fill-column
+(defvar popup-imenu-position 'point
   "Defines popup position.  Possible values are one of:
+'point - open popup at point. (default option)
 'center - opens popup at window center
-'fill-column - center relative to `fill-column'
-'point - open popup at point.")
+'fill-column - center relative to `fill-column'")
+
+(defvar popup-imenu-force-position nil
+  "When popup position, as calculated according to 'center or 'fill-column settings,
+points to a line that is not long enough, then popup will not be open at
+'center or 'fill-column position.
+Setting this var to `t' will add whitespaces at the end of the line to reach the column.")
 
 (defun popup-imenu--filter ()
   "Function that return either flx or a regular filtering function."
@@ -121,17 +126,22 @@ POPUP-ITEMS - items to be shown in the popup."
   (if (eq popup-imenu-position 'point)
       (point)
     (let* ((line-number (save-excursion
-                         (goto-char (window-start))
-                         (line-number-at-pos)))
-          (x (+ (/ (- (if (eq popup-imenu-position 'center) (window-width) fill-column)
-                      (apply 'max (mapcar (lambda (z) (length (car z))) popup-items)))
-                   2)
-                (window-hscroll)))
-          (y (+ (- line-number 2)
-                (/ (- (window-height) menu-height) 2))))
-      (save-excursion
-        (artist-move-to-xy x y)
-        (point)))))
+                          (goto-char (window-start))
+                          (line-number-at-pos)))
+           (x (+ (/ (- (if (eq popup-imenu-position 'center) (window-width) fill-column)
+                       (apply 'max (mapcar (lambda (z) (length (car z))) popup-items)))
+                    2)
+                 (window-hscroll)))
+           (y (+ (- line-number 2)
+                 (/ (- (window-height) menu-height) 2))))
+      (popup-imenu--point-at-col-row x y)
+      )))
+
+(defun popup-imenu--point-at-col-row (column row)
+  (save-excursion
+    (goto-line row)
+    (move-to-column column popup-imenu-force-position)
+    (point)))
 
 ;;;###autoload
 (defun popup-imenu ()
