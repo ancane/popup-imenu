e78428e9e0d13e3dc972e256aa9d1825525aed20
Chunyang Xu <xuchunyang56@gmail.com>
2016-02-14 21:01:22 +0800

    Fix URL

--- popup-imenu.el
+++ popup-imenu.el
@@ -4,7 +4,7 @@
 ;; Version: 0.3
 ;; Package-Requires: ((dash "2.12.1") (popup "0.5.3") (flx-ido "0.6.1"))
 ;; Keywords: popup, imenu
-;; URL: https://github.com/ancane/popup-imenu.el
+;; URL: https://github.com/ancane/popup-imenu
 
 ;; This file is NOT part of GNU Emacs.
 
